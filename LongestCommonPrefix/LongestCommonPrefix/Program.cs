c2b5701f3ef9d538e08c86e9c9fc600b528eaec9
iislommm <ibragimegamberdiyev81@gmail.com>
2025-08-12 13:16:43 +0500

    uploaded

---
+++ LongestCommonPrefix/LongestCommonPrefix/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        var result = "";
+        var shortest = strs.OrderBy(s => s.Length).First();
+        for (var i = 0; i < shortest.Length; i++)
+        {
+            var s = shortest[i];
+            foreach (var str in strs)
+            {
+                if (str[i] != s)
+                {
+                    return result;
+                }
+            }
+            result += s;
+        }
+        return result;
+    }
+}
