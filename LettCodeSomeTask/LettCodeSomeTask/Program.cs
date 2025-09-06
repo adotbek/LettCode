505e870d379eb777a8f55f20a854469631d39121
iislommm <ibragimegamberdiyev81@gmail.com>
2025-09-06 10:24:11 +0500

    updated

---
+++ LettCodeSomeTask/LettCodeSomeTask/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Solution
+{
+    public string ReverseStr(string s, int k)
+    {
+        var list = new List<string>();
+        var result = string.Empty;
+        var addS = string.Empty;
+
+        foreach (var ch in s)
+        {
+            addS += ch;
+            if (addS.Length == k)
+            {
+                list.Add(addS);
+                addS = string.Empty;
+            }
+        }
+        if (addS.Length > 0)
+        {
+            list.Add(addS);
+        }
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (i % 2 == 0)
+            {
+                result += new string(list[i].Reverse().ToArray());
+            }
+            else
+            {
+                result += list[i];
+            }
+        }
+
+        return result;
+    }
+}
