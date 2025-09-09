12342bd5e90a2d59bc4eedbc54c1f8388378a57a
iislommm <ibragimegamberdiyev81@gmail.com>
2025-09-09 10:12:45 +0500

    uploaded

---
+++ ConstructSmallestNumberFromDiString/ConstructSmallestNumberFromDiString/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Solution
+{
+    public string SmallestNumber(string pattern)
+    {
+        var helper = "";
+        var result = "";
+        for (var i = 0; i < pattern.Length; i ++)
+        {
+            if ()
+        }
+    }
+}
---
+++ LengthOfTheLastWord/LengthOfTheLastWord/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        s= s.Trim();
+        var result = s.Split(" ");
+        return result[result.Length - 1].Length;
+    }
+}
