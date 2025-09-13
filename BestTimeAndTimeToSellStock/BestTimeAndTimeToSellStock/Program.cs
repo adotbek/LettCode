ca156915aa1b3969cbfea6903e7d7cc2ba75c525
iislommm <ibragimegamberdiyev81@gmail.com>
2025-09-13 09:53:15 +0500

    E

---
+++ BestTimeAndTimeToSellStock/BestTimeAndTimeToSellStock/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        var result = 0;
+        var min = int.MaxValue;
+        var max = 0;
+        foreach (var price in prices)
+        {
+            if (price < min) min = price;
+            result = price - min;
+            if (price > min) max = price;
+        }
+        return max;
+    }
+}
---
+++ SplitAStringInABalancedString/SplitAStringInABalancedString/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public int BalancedStringSplit(string s)
+    {
+        //"RLRRLLRLRL"
+        var rCount = 0;
+        var lCount = 0;
+        var result = 0;
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] == 'R')
+            {
+                rCount++;
+            }
+            else
+            {
+                lCount++;
+            }
+            if (rCount == lCount)
+            {
+                result++;
+                rCount = 0;
+                lCount = 0;
+            }
+        }
+        return result;
+    }
+}
