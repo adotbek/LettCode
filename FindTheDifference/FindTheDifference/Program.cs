6dd55ad351578f602c002fb120a1f6a0ac7cbca4
iislommm <ibragimegamberdiyev81@gmail.com>
2025-09-17 10:41:03 +0500

    E

---
+++ FindTheDifference/FindTheDifference/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Solution
+{
+    public char FindTheDifference(string s, string t)
+    {
+        foreach(var ch in t)
+        {
+            if (!s.Contains(ch) || t.Count(x=>x==ch) > s.Count(x=>x==ch)) return ch;
+        }
+        return 'o';
+    }
+}
