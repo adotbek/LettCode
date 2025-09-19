89aff4339b49a08277f57b744c0d6c277963ddca
iislommm <ibragimegamberdiyev81@gmail.com>
2025-09-19 11:59:52 +0500

    E

---
+++ AddDigits/AddDigits/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public int AddDigits(int num)
+    {
+        while (num >= 10)
+        {
+            int result = 0;
+            int temp = num;
+
+           
+            while (temp > 0)
+            {
+                result += temp % 10;
+                temp /= 10;
+            }
+
+            num = result;
+        }
+
+        return num;
+    }
+}
