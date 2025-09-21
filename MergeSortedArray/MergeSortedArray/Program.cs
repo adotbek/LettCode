af1ebae65ebc85fef18d9b47d9cb3693cfcc3338
iislommm <ibragimegamberdiyev81@gmail.com>
2025-09-21 15:54:40 +0500

    E

---
+++ MergeSortedArray/MergeSortedArray/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Solution
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int [] result = nums1.Take(m).Concat(nums2.Take(n)).ToArray();
+        Array.Sort(result);
+        for (var i = 0; i < result.Length; i++)
+        {
+            nums1[i] = result[i];
+        }
+    }
+}
