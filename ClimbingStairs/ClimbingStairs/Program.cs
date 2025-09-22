0a0d7e3f2e755758cb6338e476ef6b8a1e56fd8b
iislommm <ibragimegamberdiyev81@gmail.com>
2025-09-22 10:54:54 +0500

    E

---
+++ ClimbingStairs/ClimbingStairs/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public int ClimbStairs(int n)
+    {
+        if (n is 1) return n;
+        if (n is 2) return n;
+
+        var first = 1;
+        var second = 2;
+        var result = 0;
+        for (var i = 3; i <= n; i++)
+        {
+            result = first + second;
+            first = second;
+            second = result;
+        }
+        return second;
+    }
+}
