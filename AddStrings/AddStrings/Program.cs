98253518787558d39ac2b8f891051fffbf25ef51
iislommm <ibragimegamberdiyev81@gmail.com>
2025-10-06 14:02:38 +0500

    E

---
+++ AddStrings/AddStrings/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Solution
+{
+    public string AddStrings(string num1, string num2)
+    {
+        var int1 = int.Parse(num1);
+        var int2 = int.Parse(num2);
+
+        var result = int1 + int2;
+
+        return result.ToString(); 
+    }
+}
---
+++ SortingTheSentence/SortingTheSentence/Program.cs
@@ -0,0 +1,19 @@
+﻿    public class Solution
+    {
+        public string SortSentence(string s)
+        {
+            var splitedArray = s.Split(' ');
+            var result = new string[splitedArray.Length];
+
+            for (var i = 0; i < splitedArray.Length; i++)
+            {
+ 
+                var word = splitedArray[i];
+                var index = word[^1] - '0';
+                var gap = word[..^1];
+
+                result[index - 1] = gap;
+            }
+            return string.Join(" ", result);
+        }
+    }
