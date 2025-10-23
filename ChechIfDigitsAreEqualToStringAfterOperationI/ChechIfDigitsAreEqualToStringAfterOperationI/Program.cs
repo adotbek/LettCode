d4b3aa16ec9d778547e559640695c94a03a0efe8
iislommm <ibragimegamberdiyev81@gmail.com>
2025-10-23 10:45:38 +0500

    E

---
+++ ChechIfDigitsAreEqualToStringAfterOperationI/ChechIfDigitsAreEqualToStringAfterOperationI/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public bool HasSameDigits(string s)
+    {
+        var s1 = string.Empty;
+        for (var i = 0; i < s.Length - 1; i++)
+        {
+            if (s.Length <= 2)
+            {
+                break;
+            }
+            var a = s[i] - '0' + s[i + 1] - '0';
+            s1 = s1 + $"{a%10}";
+            if (i + 2 == s.Length)
+            {
+                s = s1;
+                s1 = string.Empty;
+                i = -1;
+            }
+        }
+        return s[0] == s[1];
+
+    }
+}
