7d6a2096d08c7ef044fce164dfeb2aac99d95f78
iislommm <ibragimegamberdiyev81@gmail.com>
2025-10-25 12:44:05 +0500

    E

---
+++ CalculateMoneyInLeetcodeBank/CalculateMoneyInLeetcodeBank/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int TotalMoney(int n)
+    {
+        var result = 0;
+        var a = 0;
+        var j = 1;
+        for (var i = 1; i <= n; i++)
+        {
+            if (i % 7 == 1 && i != 1)
+            {
+                a++;
+                j = a;
+            }
+            result += j;
+            j++;
+        }
+        return result;
+    }
+}
